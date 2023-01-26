290a9f26c17b9c98c8ce3de0e6cb3abc36be7d5d
GitHub Action <action@github.com>
2023-01-26 19:08:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/fabiencdp/intellijdebugunitary/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fabiencdp/intellijdebugunitary/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fabiencdp.intellijdebugunitary.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fabiencdp.intellijdebugunitary.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/fabiencdp/intellijdebugunitary/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fabiencdp/intellijdebugunitary/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fabiencdp.intellijdebugunitary.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fabiencdp.intellijdebugunitary.MyBundle
 
 class MyProjectService(project: Project) {
 
