8e6cd21bd32b7feab10122c954e4edaec4d97a9d
Fabien <fab.gane@gmail.com>
2023-02-05 19:36:32 +0100

    Working version

--- src/main/java/inspection/DebuggerLineMarkerProvider.java
+++ src/main/java/inspection/DebuggerLineMarkerProvider.java
@@ -1,39 +1,102 @@
 package inspection;
 
+import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
 import com.intellij.icons.AllIcons;
 import com.intellij.lang.javascript.JSTokenTypes;
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.lang.javascript.psi.impl.JSFunctionImpl;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.editor.markup.GutterIconRenderer;
-import com.intellij.openapi.util.IconLoader;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.util.Function;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.MouseEvent;
 
 public class DebuggerLineMarkerProvider implements LineMarkerProvider {
     @Override
     public LineMarkerInfo<?> getLineMarkerInfo(@NotNull PsiElement element) {
-        if (!(element instanceof LeafPsiElement) || ((LeafPsiElement) element).getElementType() != JSTokenTypes.FUNCTION_KEYWORD){
+        if (
+                !(element instanceof LeafPsiElement) ||
+                ((LeafPsiElement) element).getElementType() != JSTokenTypes.FUNCTION_KEYWORD ||
+                !((LeafPsiElement) element).canNavigate()
+        )
+        {
             return null;
         }
 
-        return new LineMarkerInfo(element, element.getTextRange(), AllIcons.Actions.Execute, null,null, GutterIconRenderer.Alignment.RIGHT);
+
+        GutterIconNavigationHandler<PsiElement> navHandler = new GutterIconNavigationHandler<PsiElement>() {
+            @Override
+            public void navigate(MouseEvent e, PsiElement elt) {
+                System.out.println("don't click on me");
+            }
+        };
+
+
+        return new RunLineMarkerInfo(
+                (LeafPsiElement) element,
+                AllIcons.Actions.Execute,
+                createActionGroup(element),
+                navHandler,
+                createToolTipProvider(element)
+        );
     }
 
+    @Contract(pure = true)
+    private Function createToolTipProvider(PsiElement element) {
+        return (Function) element1 -> "Tooltip calculated at ";
+    }
 
-//    public DefaultActionGroup createActionGroup(@NotNull PsiElement element) {
-////        val linkDestinationElement =
-////                findChildElement(element, MarkdownTokenTypeSets.LINK_DESTINATION, null)
-////        val linkDestination = linkDestinationElement?.text
-//        ActionGroup group = new DefaultActionGroup();
-////        group.actionPerformed();
-////        group.add(ActionManager.getInstance().getAction("MyPlugin.OpenToolWindowAction"));
-//        return group;
-//    }
+    public DefaultActionGroup createActionGroup(PsiElement element) {
+        DefaultActionGroup group = new DefaultActionGroup();
+//        group.addAction("dq");
+        return group;
+    }
+}
+
+class RunLineMarkerInfo extends LineMarkerInfo {
+    private final ActionGroup actionGroup;
+
+    private final LeafPsiElement element;
+
+    public RunLineMarkerInfo(
+            @NotNull LeafPsiElement element,
+            @NotNull Icon icon,
+            @NotNull ActionGroup actionGroup,
+            @NotNull GutterIconNavigationHandler navHandler,
+            @Nullable Function<LeafPsiElement, String> tooltipProvider
+    ) {
+        // TODO: test navHandler
+        super(element.getParent(), element.getParent().getTextRange(), icon, tooltipProvider, null, GutterIconRenderer.Alignment.RIGHT);
+        this.actionGroup = actionGroup;
+        this.element = element;
+    }
+
+    @Override
+    public GutterIconRenderer createGutterRenderer() {
+        return new LineMarkerGutterIconRenderer<>(this) {
+            @Override
+            public AnAction getClickAction() {
+                DemoAction action = new DemoAction();
+                action.setElement((JSFunctionImpl) (element).getParent());
+                return action;
+            }
+
+            @Override
+            public boolean isNavigateAction() {
+                return super.isNavigateAction();
+            }
+
+            @Override
+            public @Nullable ActionGroup getPopupMenuActions() {
+                return actionGroup;
+            }
+        };
+    }
 }
--- src/main/java/inspection/DemoAction.java
+++ src/main/java/inspection/DemoAction.java
@@ -5,24 +5,19 @@ import com.intellij.lang.Language;
 import com.intellij.lang.javascript.psi.impl.JSFunctionImpl;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.pom.Navigatable;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiFile;
-import org.apache.pdfbox.io.ScratchFile;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class DemoAction extends AnAction {
+    JSFunctionImpl element;
+
+    public void setElement(JSFunctionImpl element) {
+        this.element = element;
+    }
 
     @Override
     public void update(@NotNull AnActionEvent event) {
@@ -33,42 +28,33 @@ public class DemoAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
+        if (!element.canNavigate()) {
+            return;
+        }
         // Using the event, create and show a dialog
         Project currentProject = event.getProject();
 
-        StringBuilder message =
-                new StringBuilder(event.getPresentation().getText() + " Selected!");
         // If an element is selected in the editor, add info about it.
-        Navigatable selectedElement = event.getData(CommonDataKeys.NAVIGATABLE);
-        if (selectedElement != null && selectedElement.canNavigate()) {
-            try {
-                String extension = ((JSFunctionImpl) selectedElement).getContainingFile().getFileType().getDefaultExtension();
-                Language language = ((JSFunctionImpl) selectedElement).getLanguage();
-                String fileName = ((JSFunctionImpl<?>) selectedElement).getName().concat("." + extension);
-                String text = ((JSFunctionImpl<?>) selectedElement).getText();
-                ScratchRootType.getInstance().createScratchFile(currentProject, fileName, language, text);
+        // Navigatable selectedElement = event.getData(CommonDataKeys.NAVIGATABLE);
 
-//                String directory = event.getData(CommonDataKeys.VIRTUAL_FILE).getParent().getPath();
-////                Writer myWriter = Files.newBufferedWriter(Paths.get(directory, "test-2.js"), Charset.defaultCharset());
-//                Writer myWriter = Files.newBufferedWriter(Paths.get("/home/fabien/www/OTHERS/jetbrains/intellij-debug-unitary/build/idea-sandbox/config/scratches/test.js"), Charset.defaultCharset());
-//
-//                myWriter.write(((JSFunctionImpl<?>) selectedElement).getText());
-//
-//                myWriter.close();
-            } catch (Exception e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
+        try {
+            String extension = this.element.getContainingFile().getFileType().getDefaultExtension();
+            Language language = this.element.getLanguage();
+            String fileName = this.element.getName().concat("." + extension);
+            String text = this.element.getText();
 
-            message.append("\nSelected Element: ").append(selectedElement);
+            VirtualFile file = ScratchRootType
+                    .getInstance()
+                    .createScratchFile(currentProject, fileName, language, text);
+
+            FileEditorManager.getInstance(currentProject).openTextEditor(
+                new OpenFileDescriptor(currentProject, file),
+                true // request focus to editor
+            );
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
 
-
-        String title = event.getPresentation().getDescription();
-//        Messages.showMessageDialog(
-//                currentProject,
-//                message.toString(),
-//                title,
-//                Messages.getInformationIcon());
     }
 }
